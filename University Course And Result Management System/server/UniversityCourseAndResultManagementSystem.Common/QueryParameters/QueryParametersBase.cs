d67feeb048ae50eb4754f7694e61175a88bd5c29
Tonmoy-Ulrich <tonmoyulrich@gmail.com>
2022-12-08 13:03:14 +0600

    repository base

---
+++ University Course And Result Management System/server/UniversityCourseAndResultManagementSystem.Common/QueryParameters/QueryParametersBase.cs
@@ -0,0 +1,48 @@
+﻿namespace UniversityCourseAndResultManagementSystem.Common.QueryParameters
+{
+    public abstract class QueryParametersBase
+    {
+        const int maxPageSize = 100;
+
+        private int _pageNumber = 1;
+        public int PageNumber
+        {
+            get
+            {
+                return _pageNumber;
+            }
+            set
+            {
+                _pageNumber = (value < 1) ? 1 : value;
+            }
+        }
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                if (value > maxPageSize)
+                {
+                    _pageSize = maxPageSize;
+                }
+                else if (value < 1)
+                {
+                    _pageSize = 1;
+                }
+                else
+                {
+                    _pageSize = value;
+                }
+
+            }
+        }
+
+        public string OrderBy { get; set; } = "Id";
+        public string Fields { get; set; } = "";
+    }
+}
---
+++ University Course And Result Management System/server/UniversityCourseAndResultManagementSystem.Repository/RepositoryBase.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UniversityCourseAndResultManagementSystem.Common.QueryParameters;
+using UniversityCourseAndResultManagementSystem.Repository.Contracts;
+
+namespace UniversityCourseAndResultManagementSystem.Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet;
+
+        public RepositoryBase(DbContext employeeManagementSystemContext)
+        {
+            _dbSet = employeeManagementSystemContext.Set<T>();
+        }
+
+        public IQueryable<T> GetAllNoTracking()
+        {
+            return _dbSet.AsNoTracking();
+        }
+
+        public IQueryable<T> GetAllNoTrackingWithParam(QueryParametersBase param, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy)
+        {
+            return orderBy(_dbSet
+                .Skip((param.PageNumber - 1) * param.PageSize)
+                .Take(param.PageSize)
+                .AsNoTracking());
+        }
+
+        public IQueryable<T> GetByConditionNoTracking(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression).AsNoTracking();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddAsyncRange(IEnumerable<T> entity)
+        {
+            await _dbSet.AddRangeAsync(entity);
+        }
+
+        public async Task Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+        public async Task UpdateRange(IEnumerable<T> entity)
+        {
+            _dbSet.UpdateRange(entity);
+        }
+
+        public async Task Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+        public async Task DeleteRange(IEnumerable<T> entity)
+        {
+            _dbSet.RemoveRange(entity);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
+
+        public async Task<int> CountAllAsync()
+        {
+            return await _dbSet.AsNoTracking().CountAsync();
+        }
+
+        public async Task<int> CountByConditionAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AsNoTracking().CountAsync(expression);
+        }
+    }
+}
