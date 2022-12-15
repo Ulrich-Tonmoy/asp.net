1aacb9673f1b75aa0a92a7348235c7043afdcd2c
Tonmoy-Ulrich <tonmoyulrich@gmail.com>
2022-12-15 19:30:07 +0600

    GlobalConstants added

---
+++ University Course And Result Management System/server/UniversityCourseAndResultManagementSystem.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversityCourseAndResultManagementSystem.Common
+{
+    public class GlobalConstants
+    {
+        public const string SERVER_ERROR = "Internal server error!";
+        public const string OBJECT_NULL = "{0} oject is null!";
+        public const string SUCCESSFULLY_DELETED = "Successfully deleted {0} data";
+    }
+}
