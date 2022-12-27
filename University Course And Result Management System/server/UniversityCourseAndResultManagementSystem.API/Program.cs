7d0ca09d7107b8b090d842d334d7c140e0771649
Tonmoy-Ulrich <tonmoyulrich@gmail.com>
2022-12-27 18:43:33 +0600

    cors fixed

--- University Course And Result Management System/server/UniversityCourseAndResultManagementSystem.API/Program.cs
+++ University Course And Result Management System/server/UniversityCourseAndResultManagementSystem.API/Program.cs
@@ -67,6 +67,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseAuthorization();
 
 app.MapControllers();
