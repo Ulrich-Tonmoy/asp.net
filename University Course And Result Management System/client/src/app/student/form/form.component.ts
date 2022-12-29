f2c31e89779d89ae3617125991851018b0cf093a
Tonmoy-Ulrich <tonmoyulrich@gmail.com>
2022-12-29 16:45:43 +0600

    student crud

---
+++ University Course And Result Management System/client/src/app/student/form/form.component.ts
@@ -0,0 +1,150 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
+import {
+    FormBuilder,
+    FormGroup,
+    Validators,
+    FormControl,
+    FormGroupDirective,
+    NgForm,
+} from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+@Component({
+    selector: 'student-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.scss'],
+})
+export class StudentFormComponent {
+    studentForm!: FormGroup;
+    actionButton: string = 'Register';
+    formName: string = 'Add New Student';
+
+    deptData!: any;
+
+    // email input validation
+    emailFormControl = new FormControl('', [
+        Validators.required,
+        Validators.email,
+    ]);
+    matcher = new MyErrorStateMatcher();
+
+    // date picker
+    currentDate = new FormControl(new Date());
+
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public editData: any,
+        private http: HttpClient,
+        private formBuilder: FormBuilder,
+        private dialogRef: MatDialogRef<StudentFormComponent>
+    ) {}
+
+    ngOnInit(): void {
+        this.studentForm = this.formBuilder.group({
+            regiNo: ['', Validators.required],
+            name: ['', Validators.required],
+            contactNo: ['', Validators.required],
+            address: ['', Validators.required],
+            departmentId: ['', Validators.required],
+        });
+
+        this.getDepartment();
+
+        if (this.editData) {
+            this.actionButton = 'Update';
+            this.formName = `Update Student "${this.editData.regiNo}"`;
+
+            this.studentForm.controls['regiNo'].setValue(this.editData.regiNo);
+            this.studentForm.controls['name'].setValue(this.editData.name);
+            this.emailFormControl = new FormControl(this.editData.email, [
+                Validators.required,
+                Validators.email,
+            ]);
+            this.studentForm.controls['contactNo'].setValue(
+                this.editData.contactNo
+            );
+            this.currentDate = new FormControl(this.editData.date);
+            this.studentForm.controls['address'].setValue(
+                this.editData.address
+            );
+            this.studentForm.controls['departmentId'].setValue(
+                this.editData.departmentId
+            );
+        }
+    }
+
+    getDepartment() {
+        this.http.get('https://localhost:7026/api/department').subscribe({
+            next: (data) => {
+                const newData: any = data;
+                this.deptData = newData.data;
+            },
+            error: (err) => {
+                console.log(err.error);
+            },
+        });
+    }
+
+    formAction() {
+        this.studentForm.value.email = this.emailFormControl.value;
+        this.studentForm.value.date = this.currentDate.value?.toISOString();
+        if (this.editData) {
+            this.updateStudent();
+        } else {
+            this.createStudent();
+        }
+    }
+    createStudent() {
+        if (this.studentForm.valid) {
+            this.http
+                .post(
+                    'https://localhost:7026/api/student',
+                    this.studentForm.value
+                )
+                .subscribe({
+                    next: (data) => {
+                        this.studentForm.reset();
+                        this.dialogRef.close('save');
+                    },
+                    error: (err) => {
+                        console.log(err);
+                    },
+                });
+        }
+    }
+
+    updateStudent() {
+        if (this.studentForm.valid) {
+            this.http
+                .put('https://localhost:7026/api/student', {
+                    ...this.studentForm.value,
+                    id: this.editData.id,
+                })
+                .subscribe({
+                    next: (data) => {
+                        this.studentForm.reset();
+                        this.dialogRef.close('update');
+                    },
+                    error: (err) => {
+                        console.log(err.error);
+                    },
+                });
+        }
+    }
+}
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(
+        control: FormControl | null,
+        form: FormGroupDirective | NgForm | null
+    ): boolean {
+        const isSubmitted = form && form.submitted;
+        return !!(
+            control &&
+            control.invalid &&
+            (control.dirty || control.touched || isSubmitted)
+        );
+    }
+}
--- University Course And Result Management System/client/src/app/student/student.component.ts
+++ University Course And Result Management System/client/src/app/student/student.component.ts
@@ -1,38 +1,78 @@
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { StudentFormComponent } from './form/form.component';
 
 @Component({
     selector: 'app-student',
     templateUrl: './student.component.html',
     styleUrls: ['./student.component.scss'],
 })
-export class StudentComponent implements AfterViewInit {
-    constructor(private http: HttpClient) {}
+export class StudentComponent {
+    displayedColumns: string[] = [
+        'regiNo',
+        'name',
+        'department',
+        'email',
+        'address',
+        'contactNo',
+        'date',
+        'actions',
+    ];
+    dataSource!: MatTableDataSource<any>;
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+    constructor(private http: HttpClient, public dialog: MatDialog) {}
 
     ngOnInit(): void {
         this.getStudent();
     }
 
-    displayedColumns: string[] = ['code', 'name'];
-    dataSource = new MatTableDataSource<any>([]);
-
-    @ViewChild(MatPaginator) paginator: any = MatPaginator;
-
-    ngAfterViewInit() {
-        this.dataSource.paginator = this.paginator;
+    openDialog() {
+        this.dialog
+            .open(StudentFormComponent, { width: '40%' })
+            .afterClosed()
+            .subscribe((res) => {
+                if (res == 'save') this.getStudent();
+            });
     }
 
     getStudent() {
-        this.http
-            .get('https://localhost:7026/api/student')
-            .subscribe((data) => {
-                console.log(data);
-
+        this.http.get('https://localhost:7026/api/student').subscribe({
+            next: (data) => {
                 const newData: any = data;
-                this.dataSource = newData.data;
+                this.dataSource = new MatTableDataSource(newData.data);
                 this.dataSource.paginator = this.paginator;
+            },
+            error: (err) => {
+                console.log(err.error);
+            },
+        });
+    }
+
+    editStudent(element: any) {
+        this.dialog
+            .open(StudentFormComponent, { width: '40%', data: element })
+            .afterClosed()
+            .subscribe((res) => {
+                if (res == 'update') this.getStudent();
             });
+    }
+
+    deleteStudent(element: any) {
+        if (confirm(`Do you want to delete Student "${element.regiNo}"`)) {
+            this.http
+                .delete('https://localhost:7026/api/student/' + element.id)
+                .subscribe({
+                    next: (data) => {
+                        this.getStudent();
+                    },
+                    error: (err) => {
+                        console.log(err.error);
+                    },
+                });
+        }
     }
 }
