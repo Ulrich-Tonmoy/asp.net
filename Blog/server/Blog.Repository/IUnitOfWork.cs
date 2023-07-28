19ca3d9cd399d6d20bc953035704da53eb44f249
Ulrich <ulrichtonmoy1@gmail.com>
2023-07-28 16:37:05 +0600

    chore: unit of work added

---
+++ Blog/server/Blog.Repository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Repository.Repositories.Contracts;
+
+namespace Blog.Repository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository CategoryRepository { get; set; }
+
+        Task<int> SaveAsync();
+    }
+}
---
+++ Blog/server/Blog.Repository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Repository.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbContext _blogContext;
+
+        public ICategoryRepository CategoryRepository { get; set; }
+
+        public UnitOfWork(
+            DbContext blogContext,
+            ICategoryRepository categoryRepository
+            )
+        {
+            _blogContext = blogContext;
+            CategoryRepository = categoryRepository;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _blogContext.SaveChangesAsync();
+        }
+    }
+}
