bf549b1e21450badf0781aaeda115f950885fb5e
Ulrich <ulrichtonmoy1@gmail.com>
2023-07-28 17:38:06 +0600

    fix: typo fixed

--- Blog/server/Blog.Common/GlobalConstants.cs
+++ Blog/server/Blog.Common/GlobalConstants.cs
@@ -4,7 +4,7 @@
     {
         public const string SERVER_ERROR = "Internal server error!";
         public const string OBJECT_NULL = "{0} oject is null!";
-        public const string OBJECT_Exist = "{0} oject with same {1} already exist!";
+        public const string OBJECT_EXIST = "{0} oject with same {1} already exist!";
         public const string SUCCESSFULLY_DELETED = "Successfully deleted {0} data.";
         public const string SUCCESSFULLY_UPDATED = "Successfully updated {0} data.";
     }
