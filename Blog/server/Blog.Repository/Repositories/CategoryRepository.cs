0eeaf55dc8a170bbe727ffecba77b339314aa8ff
Ulrich <ulrichtonmoy1@gmail.com>
2023-07-28 16:15:42 +0600

    chore: category repository created

---
+++ Blog/server/Blog.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Model;
+using Blog.Repository.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Repository.Repositories
+{
+    public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+    {
+        public CategoryRepository(DbContext blogContext) : base(blogContext) { }
+    }
+}
---
+++ Blog/server/Blog.Repository/Repositories/Contracts/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Blog.Model;
+
+namespace Blog.Repository.Repositories.Contracts
+{
+    public interface ICategoryRepository : IRepositoryBase<Category>
+    {
+    }
+}
