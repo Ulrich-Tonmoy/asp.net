a87dada42231e0ba61c2f3bfc59fa51ad2c5d4cf
Ulrich <ulrichtonmoy1@gmail.com>
2023-07-28 17:15:05 +0600

    chore: auto mapper added

---
+++ Blog/server/Blog.Common/MappingProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Blog.DTO.Category;
+using Blog.Model;
+
+namespace Blog.Common
+{
+    public static class Mapping
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                // This line ensures that internal properties are also mapped over.
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<MappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CategoryCreateDTO, Category>();
+            CreateMap<CategoryUpdateDTO, Category>();
+            CreateMap<Category, CategoryResponseDTO>();
+        }
+    }
+}
