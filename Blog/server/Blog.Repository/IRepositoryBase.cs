b686ae72978a4b18454cb6a9876558a7d25879a4
Ulrich <ulrichtonmoy1@gmail.com>
2023-07-28 16:15:29 +0600

    chore: repository base created

---
+++ Blog/server/Blog.Repository/IRepositoryBase.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Common.QueryParameters;
+using System.Linq.Expressions;
+
+namespace Blog.Repository
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> GetAllNoTracking();
+        IQueryable<T> GetAllNoTrackingWithParam(QueryParametersBase param, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy);
+        IQueryable<T> GetByConditionNoTracking(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddAsyncRange(IEnumerable<T> entity);
+        Task Update(T entity);
+        Task UpdateRange(IEnumerable<T> entity);
+        Task Delete(T entity);
+        Task DeleteRange(IEnumerable<T> entity);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task<int> CountAllAsync();
+        Task<int> CountByConditionAsync(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ Blog/server/Blog.Repository/RepositoryBase.cs
@@ -0,0 +1,77 @@
+﻿using Blog.Common.QueryParameters;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Blog.Repository
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet;
+
+        public RepositoryBase(DbContext blogContext)
+        {
+            _dbSet = blogContext.Set<T>();
+        }
+
+        public IQueryable<T> GetAllNoTracking()
+        {
+            return _dbSet.AsNoTracking();
+        }
+
+        public IQueryable<T> GetAllNoTrackingWithParam(QueryParametersBase param, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy)
+        {
+            return orderBy(_dbSet
+                .Skip((param.PageNumber - 1) * param.PageSize)
+                .Take(param.PageSize)
+                .AsNoTracking());
+        }
+
+        public IQueryable<T> GetByConditionNoTracking(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression).AsNoTracking();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddAsyncRange(IEnumerable<T> entity)
+        {
+            await _dbSet.AddRangeAsync(entity);
+        }
+
+        public async Task Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+        public async Task UpdateRange(IEnumerable<T> entity)
+        {
+            _dbSet.UpdateRange(entity);
+        }
+
+        public async Task Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+        public async Task DeleteRange(IEnumerable<T> entity)
+        {
+            _dbSet.RemoveRange(entity);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
+
+        public async Task<int> CountAllAsync()
+        {
+            return await _dbSet.AsNoTracking().CountAsync();
+        }
+
+        public async Task<int> CountByConditionAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AsNoTracking().CountAsync(expression);
+        }
+    }
+}
