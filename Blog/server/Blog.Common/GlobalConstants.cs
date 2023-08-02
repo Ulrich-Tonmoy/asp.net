7c1151a88c63a6994a3e08a0e28353b784e5b167
Ulrich <ulrichtonmoy1@gmail.com>
2023-08-02 18:23:57 +0600

    :pencil2: fix: constant typos fixed

--- Blog/server/Blog.Common/GlobalConstants.cs
+++ Blog/server/Blog.Common/GlobalConstants.cs
@@ -3,9 +3,9 @@
     public class GlobalConstants
     {
         public const string SERVER_ERROR = "Internal server error!";
-        public const string OBJECT_NULL = "{0} oject is null!";
-        public const string OBJECT_EXIST = "{0} oject with same {1} already exist!";
-        public const string OBJECT_DOESNOT_EXIST = "{0} oject doesn't exist!";
+        public const string OBJECT_NULL = "{0} object is null!";
+        public const string OBJECT_EXIST = "{0} object with same {1} already exist!";
+        public const string OBJECT_DOESNOT_EXIST = "{0} object doesn't exist!";
         public const string SUCCESSFULLY_DELETED = "Successfully deleted {0} data.";
         public const string SUCCESSFULLY_UPDATED = "Successfully updated {0} data.";
         public const string WRONG_PASSWORD = "Wrong Password!";
