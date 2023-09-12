05687332707a91b70bea40dc988c3bbdbddbe01d
Ulrich <ulrichtonmoy1@gmail.com>
2023-09-12 19:27:11 +0600

    chore: base response class

---
+++ LeaveManagement/LeaveManagement.Application/Responses/BaseCommandResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagement.Application.Responses
+{
+    public class BaseCommandResponse
+    {
+        public Guid Id { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
