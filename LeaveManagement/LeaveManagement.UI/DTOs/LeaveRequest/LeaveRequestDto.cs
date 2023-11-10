b06c6ab2d37d7ac31dacf3ff7bcc60cf18a5fe61
Ulrich <ulrichtonmoy1@gmail.com>
2023-11-10 18:05:42 +0600

    chore: leave request dto and service for blazor

---
+++ LeaveManagement/LeaveManagement.UI/DTOs/LeaveRequest/LeaveRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagement.UI.DTOs.Common;
+using LeaveManagement.UI.DTOs.LeaveType;
+
+namespace LeaveManagement.UI.DTOs.LeaveRequest
+{
+    public class LeaveRequestDto : BaseDto
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LeaveTypeDto LeaveType { get; set; }
+        public Guid LeaveTypeId { get; set; }
+        public DateTime DateRequested { get; set; }
+        public string RequestComments { get; set; }
+        public DateTime? DateActioned { get; set; }
+        public bool? Approved { get; set; }
+        public bool Cancelled { get; set; }
+    }
+}
---
+++ LeaveManagement/LeaveManagement.UI/Services/IServices/ILeaveRequestService.cs
@@ -0,0 +1,13 @@
+﻿using LeaveManagement.UI.DTOs.LeaveRequest;
+
+namespace LeaveManagement.UI.Services.IServices
+{
+    public interface ILeaveRequestService
+    {
+        Task<List<LeaveRequestDto>> GetAllLeaveRequest();
+        Task<LeaveRequestDto> GetLeaveRequestById(Guid id);
+        Task<Response<Guid>> CreateLeaveRequest(LeaveRequestDto leaverequest);
+        Task<Response<Guid>> DeleteLeaveRequest(Guid id);
+        Task<Response<Guid>> UpdateLeaveRequest(LeaveRequestDto leaverequest);
+    }
+}
