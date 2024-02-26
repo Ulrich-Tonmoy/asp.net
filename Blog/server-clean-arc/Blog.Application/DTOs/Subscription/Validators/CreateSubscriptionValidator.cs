3cbb80e92320e35f5cb511d8fd77e04fa4b3e8ee
Ulrich <ulrichtonmoy1@gmail.com>
2024-02-26 21:18:28 +0600

    subscription validator added

---
+++ Blog/server-clean-arc/Blog.Application/DTOs/Subscription/Validators/CreateSubscriptionValidator.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Application.IRepository;
+using FluentValidation;
+
+namespace Blog.Application.DTOs.Subscription.Validators
+{
+    public class CreateSubscriptionValidator : AbstractValidator<CreateSubscriptionDto>
+    {
+        public CreateSubscriptionValidator(ISubscriptionRepository subRepository)
+        {
+            RuleFor(l => l.Name).NotNull();
+            RuleFor(l => l.Email).MustAsync(async (email, token) =>
+            {
+                var exist = await subRepository.Exists(u => u.Email.Equals(email));
+                return !exist;
+            });
+        }
+    }
+}
---
+++ Blog/server-clean-arc/Blog.Application/DTOs/Subscription/Validators/UpdateSubscriptionValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Blog.Application.DTOs.Subscription.Validators
+{
+    public class UpdateSubscriptionValidator : AbstractValidator<UpdateSubscriptionDto>
+    {
+        public UpdateSubscriptionValidator()
+        {
+            RuleFor(l => l.Name).NotNull();
+            RuleFor(l => l.Email).NotNull();
+        }
+    }
+}
