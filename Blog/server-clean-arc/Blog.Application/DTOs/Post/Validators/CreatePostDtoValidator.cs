68b21c4813fddf30f839998cba1608885f5088b0
Ulrich <ulrichtonmoy1@gmail.com>
2024-02-26 21:25:01 +0600

    post validator added

---
+++ Blog/server-clean-arc/Blog.Application/DTOs/Post/Validators/CreatePostDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Application.IRepository;
+using FluentValidation;
+
+namespace Blog.Application.DTOs.Post.Validators
+{
+    public class CreatePostDtoValidator : AbstractValidator<CreatePostDto>
+    {
+        public CreatePostDtoValidator(ICategoryRepository catRepository)
+        {
+            RuleFor(v => v.Title).NotNull();
+            RuleFor(v => v.PermaLink).NotNull();
+            RuleFor(v => v.HeroImg).NotNull();
+            RuleFor(v => v.Excerpt).NotNull();
+            RuleFor(v => v.Content).NotNull();
+            RuleFor(l => l.CategoryId).MustAsync(async (CategoryId, token) =>
+            {
+                var exist = await catRepository.Exists(u => u.Id.Equals(CategoryId));
+                return !exist;
+            });
+        }
+    }
+}
---
+++ Blog/server-clean-arc/Blog.Application/DTOs/Post/Validators/UpdatePostDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Application.IRepository;
+using FluentValidation;
+
+namespace Blog.Application.DTOs.Post.Validators
+{
+    public class UpdatePostDtoValidator : AbstractValidator<UpdatePostDto>
+    {
+        public UpdatePostDtoValidator(ICategoryRepository catRepository)
+        {
+            RuleFor(v => v.Title).NotNull();
+            RuleFor(v => v.PermaLink).NotNull();
+            RuleFor(v => v.HeroImg).NotNull();
+            RuleFor(v => v.Excerpt).NotNull();
+            RuleFor(v => v.Content).NotNull();
+            RuleFor(l => l.CategoryId).MustAsync(async (CategoryId, token) =>
+            {
+                var exist = await catRepository.Exists(u => u.Id.Equals(CategoryId));
+                return !exist;
+            });
+        }
+    }
+}
