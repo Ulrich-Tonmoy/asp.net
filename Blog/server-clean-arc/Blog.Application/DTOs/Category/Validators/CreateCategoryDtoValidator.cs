7a8ed6bca9a839104c3703f92c58e89dc6af6419
Ulrich <ulrichtonmoy1@gmail.com>
2024-02-26 21:27:19 +0600

    category validator added

---
+++ Blog/server-clean-arc/Blog.Application/DTOs/Category/Validators/CreateCategoryDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Application.IRepository;
+using FluentValidation;
+
+namespace Blog.Application.DTOs.Category.Validators
+{
+    public class CreateCategoryDtoValidator : AbstractValidator<CreateCategoryDto>
+    {
+        public CreateCategoryDtoValidator(ICategoryRepository catRepository)
+        {
+            RuleFor(v => v.Name).NotNull().MustAsync(async (name, token) =>
+            {
+                var exist = await catRepository.Exists(u => u.Name.Equals(name));
+                return !exist;
+            });
+        }
+    }
+}
---
+++ Blog/server-clean-arc/Blog.Application/DTOs/Category/Validators/UpdateCategoryDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Application.IRepository;
+using FluentValidation;
+
+namespace Blog.Application.DTOs.Category.Validators
+{
+    public class UpdateCategoryDtoValidator : AbstractValidator<UpdateCategoryDto>
+    {
+        public UpdateCategoryDtoValidator(ICategoryRepository catRepository)
+        {
+            RuleFor(v => v.Name).NotNull().MustAsync(async (name, token) =>
+            {
+                var exist = await catRepository.Exists(u => u.Name.Equals(name));
+                return !exist;
+            });
+        }
+    }
+}
